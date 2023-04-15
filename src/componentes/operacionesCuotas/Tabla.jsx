9575f85bab7495abfb55d87b2429b104adc4f801
AlbertV99 <tico.urquhart@gmail.com>
2023-04-14 23:32:51 -0400

    Creacion inicial de panel de operaciones para cuotas.

---
+++ src/componentes/operacionesCuotas/Tabla.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Fila from './Fila'
+import Table from 'react-bootstrap/Table';
+
+const  Tabla = ({datos,ver}) => {
+
+    //// <th>Nombre</th><th>Correo</th><th>Rol</th>
+    const cabecera=["Cliente","Ingresos","Monto Credito","Interes"];
+    console.log(datos)
+  return (
+    <>
+        <Table striped hover >
+            <thead style={{backgroundColor:"#154360", color: 'white'}}>
+                <tr key="cabecera">
+                    {
+                        cabecera.map(
+                            (dato) => {
+                             return  <th key={`col-${dato}`}>{dato}</th>
+                            }
+                        )
+                    }
+
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    datos.datos.map((fila)=>{
+                        return <Fila key={"filaPanel-"+fila.id+""} dato ={fila} ver={ver}/>
+                    })
+                }
+            </tbody>
+        </Table>
+    </>
+  )
+}
+
+Tabla.propTypes = {
+    datos:PropTypes.object,
+    ver:PropTypes.func
+}
+
+export default Tabla
