cded828437efc4f3e892ad45bf899fcb99dba6e2
melig99 <56968146+melig99@users.noreply.github.com>
2023-06-04 00:25:07 -0400

    Actualiza localBD

--- src/helpers/localBD.js
+++ src/helpers/localBD.js
@@ -2,51 +2,62 @@ const localBD = () =>{
     const base =  "https://alberto.valurq.com/"
     // const base =  "http://localhost:8000/"
 
-    const iniciarSesion = async (datos)=> {
+    const iniciarSesion = async (datos) => {
         //temporal
-        let temp = await fetch(`${base}api/usuario/login`,{
-          "method": "POST",
-          "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "https://alberto.valurq.com/"
-           },
+        let temp = await fetch(`${base}api/usuario/login`, {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+            },
             "body": JSON.stringify(datos)
         });
         const data = await temp.json();
-        if(data.cod=="00"){
+        if (data.cod == "00") {
 
-            localStorage.setItem("usuario",JSON.stringify({
+            localStorage.setItem("usuario", JSON.stringify({
                 "nombre": data.success.name,
                 "perfil": data.success.perfil,
-                "token" : data.success.token,
+                "token": data.success.token,
             }));
             localStorage.removeItem("caja");
-            localStorage.setItem("menu",JSON.stringify(data.success.menu))
-            return true
-        }else{
-            return false
+            localStorage.setItem("menu", JSON.stringify(data.success.menu))
+            return data
+        } else {
+            return data
         }
     }
-    const cerrarSesion = ()=>{
+    
+    const cerrarSesion = async () => {
+        let usuario =  JSON.parse((localStorage.getItem("usuario") ?? {"token":"error"}));
+
+        let temp = await fetch(`${base}api/usuario/logout`, {
+            "method": "POST",
+            "headers": {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${usuario.token}`
+            },
+        });
         localStorage.removeItem("caja");
         localStorage.clear();
         return true
     }
-    const obtenerMenu = () =>{
+
+    const obtenerMenu = () => {
         let menu
-        if(localStorage.getItem("menu") != null){
+        if (localStorage.getItem("menu") != null) {
             menu = localStorage.getItem("menu")
-        }else{
+        } else {
             throw new Error('No existe indice');
         }
         return JSON.parse(menu)
     }
-    const obtenerUsuario = ()=>{
+    const obtenerUsuario = () => {
         let usuario
-        if(localStorage.getItem("usuario") != null){
+        if (localStorage.getItem("usuario") != null) {
             usuario = localStorage.getItem("usuario")
             console.log(usuario)
-        }else{
+        } else {
             throw new Error('No existe indice');
         }
         return JSON.parse(usuario)
@@ -54,45 +65,45 @@ const localBD = () =>{
 
 
 
-    const abrirCaja=(promesa,alerta)=>{
+    const abrirCaja = (promesa, alerta) => {
         promesa.then(
-            (a)=>{
-                if(a.cod==0){
-                    console.log(a.datos,"Caja abierta correctamente")
+            (a) => {
+                if (a.cod == 0) {
+                    console.log(a.datos, "Caja abierta correctamente")
                     alerta("Caja abierta correctamente");
-                    localStorage.setItem("caja",JSON.stringify({
-                        "caja":a.datos.id,
-                        "descripcion":a.datos.descripcion
+                    localStorage.setItem("caja", JSON.stringify({
+                        "caja": a.datos.id,
+                        "descripcion": a.datos.descripcion
                     }))
-                }else{
+                } else {
                     console.log(a)
                     alerta(a.msg);
                 }
             }
         ).catch(
-          (e)=>{
-            console.log(e)
-            alerta(e.msg);
-          }
+            (e) => {
+                console.log(e)
+                alerta(e.msg);
+            }
         )
     }
 
-    const obtenerCaja = ( )=>{
+    const obtenerCaja = () => {
         let caja
-        if(localStorage.getItem("caja") != null){
+        if (localStorage.getItem("caja") != null) {
             caja = localStorage.getItem("caja")
             console.log(caja)
-        }else{
+        } else {
             throw new Error('No existe indice');
         }
         return JSON.parse(caja)
     }
 
-    const cerrarCaja = (promesa,alerta) =>{
-        promesa.then((a)=>console.log(a)).catch((e)=>console.log(e));
+    const cerrarCaja = (promesa, alerta) => {
+        promesa.then((a) => console.log(a)).catch((e) => console.log(e));
         localStorage.removeItem("caja");
     }
 
-    return ({"obtenerUsuario":obtenerUsuario,"iniciarSesion":iniciarSesion,"cerrarSesion":cerrarSesion,"abrirCaja":abrirCaja,"obtenerCaja":obtenerCaja,"cerrarCaja":cerrarCaja,"obtenerMenu":obtenerMenu})
+    return ({ "obtenerUsuario": obtenerUsuario, "iniciarSesion": iniciarSesion, "cerrarSesion": cerrarSesion, "abrirCaja": abrirCaja, "obtenerCaja": obtenerCaja, "cerrarCaja": cerrarCaja, "obtenerMenu": obtenerMenu })
 }
 export default localBD
